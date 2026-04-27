016b38c9ecd2d47c68232627781138df8b317e76
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2026-04-27 19:56:07 +0200

    fix (workflow github): mauvaises commandes issu du refactor de l architecture

--- back/src/User.php
+++ back/src/User.php
@@ -57,11 +57,6 @@ class User extends Database {
         }
 
     }
-
-
-
-    
-
 }
 
 ?>
--- back/traitement-json.php
+++ back/traitement-json.php
@@ -1,13 +1,11 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-// Initialisation de l'environnement
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 use User\User;
 
-// On utilise des noms clairs pour ne pas écraser l'objet $user plus bas
 $dbHost = $_ENV['DB_HOST'];
 $dbName = $_ENV['DB_NAME'];
 $dbUser = $_ENV['DB_USER'];
@@ -16,18 +14,14 @@ $dbPass = $_ENV['DB_PASS'];
 header('Content-Type: application/json');
 
 try {
-    // Instanciation de la classe User (qui appelle le constructeur de Database)
     $userEntity = new User(); 
 
-    // Gestion des requêtes
     if(isset($_GET['allUsers'])){
         echo json_encode($userEntity->getUsers());
         exit;
     }
 
     if(isset($_GET['signin'])){  
-        // Attention : si c'est du JSON envoyé par fetch, $_POST sera vide !
-        // Il faut utiliser file_get_contents('php://input') si tu envoies du JSON
         $input = json_decode(file_get_contents('php://input'), true);
         $result = $userEntity->signin($input['email'], $input['password']);
         echo json_encode($result);
